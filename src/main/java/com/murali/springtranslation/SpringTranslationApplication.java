2a8b2e9b4105bde4ee234facb6dc56b95a6375cf
mkodavati <mkodavati@altimetrik.com>
2019-05-07 15:17:18 +0530

    intial commit

---
+++ src/main/java/com/murali/springtranslation/SpringTranslationApplication.java
@@ -0,0 +1,13 @@
+package com.murali.springtranslation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTranslationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTranslationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murali/springtranslation/SpringTranslationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.murali.springtranslation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTranslationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
